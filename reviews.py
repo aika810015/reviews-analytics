af5f315ae6b072c191ac5bb7df20c3dd909491b4
Rance SHIH <aika810015jp@gmail.com>
2021-03-01 10:20:27 +0900

    add filter

--- reviews.py
+++ reviews.py
@@ -12,3 +12,10 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print("平均コメントの長さは", sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print("全部は", len(new), "個長さ100以下のコメントあります。")
+print(new[0])
